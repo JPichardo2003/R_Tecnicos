f59792b31f305cd357d45287cea2292ca6851cb4
JPichardo2003 <julio_pichardo@ucne.edu.do>
2024-05-09 16:21:35 -0400

    Adding TecnicoScreen & ListScreen toMain: Also added a Resources image.

--- app/src/main/java/com/ucne/registro_tecnicos/MainActivity.kt
+++ app/src/main/java/com/ucne/registro_tecnicos/MainActivity.kt
@@ -4,44 +4,72 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.Room
+import com.ucne.registro_tecnicos.data.local.database.TecnicoDb
+import com.ucne.registro_tecnicos.data.repository.TecnicoRepository
+import com.ucne.registro_tecnicos.presentation.ticket.TecnicoListScreen
+import com.ucne.registro_tecnicos.presentation.ticket.TecnicoScreen
+import com.ucne.registro_tecnicos.presentation.ticket.TecnicoViewModel
 import com.ucne.registro_tecnicos.ui.theme.Registro_TecnicosTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var tecnicoDb: TecnicoDb
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        tecnicoDb = Room.databaseBuilder(
+            this,
+            TecnicoDb::class.java,
+            "Tecnico.db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+
+        val repository = TecnicoRepository(tecnicoDb.tecnicoDao())
         enableEdgeToEdge()
         setContent {
             Registro_TecnicosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface {
+                    val viewModel: TecnicoViewModel = viewModel(
+                        factory = TecnicoViewModel.provideFactory(repository)
                     )
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                                .padding(8.dp)
+                        ) {
+
+                            TecnicoScreen(viewModel = viewModel)
+                            TecnicoListScreen(viewModel = viewModel,
+                                onVerTecnico = {
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     Registro_TecnicosTheme {
-        Greeting("Android")
+
     }
 }
